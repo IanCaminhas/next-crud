fbd2115d0d3f628077044a3edc4aa86d8b4a4143
IanPC <iancaminhas25@hotmail.com>
2022-06-05 15:44:26 -0300

    Componente formulario e integracao formulario/tabela

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Entrada from "./Entrada";
+import Cliente from '../core/Cliente'
+import Botao from './botao'
+
+interface FormularioProps {
+    cliente: Cliente
+    clienteMudou?: (cliente: Cliente) =>void
+    cancelado?: () =>void 
+}
+
+
+export default function Formulario(props: FormularioProps) {
+    const id = props.cliente?.id
+    const [nome,SetNome] = useState(props.cliente?.nome ?? '')
+    const [idade,SetIdade] = useState(props.cliente?.idade ?? 0)
+
+    return (
+        <div>
+
+            {id ? (
+                <Entrada 
+                    somenteLeitura
+                    texto="Código" 
+                    valor={id}
+                    className="mb-5"
+                />
+            ): false}
+            <Entrada 
+                texto="Nome" 
+                valor={nome}
+                valorMudou={SetNome}
+                className="mb-5"
+            />
+            <Entrada 
+                texto="Idade" 
+                tipo="number" 
+                valor={idade}
+                valorMudou={SetIdade}
+            />
+
+            <div className="flex justify-end mt-7">
+                <Botao cor="blue" className="mr-2"
+                    onClick={() =>props.clienteMudou?.(new Cliente(nome,+idade,id))}>
+                    {id ? 'Alterar': 'Salvar'}
+                </Botao>
+                <Botao onClick={props.cancelado}>
+                    Cancelar
+                </Botao>
+
+            </div>
+            
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,43 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Botao from "../components/botao";
+import Cliente from "../core/Cliente";
+import Formulario from "../components/Formulario";
+import { useState } from "react";
 
 export default function Home() {
+  
+  const [cliente,setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel,setVisivel] = useState<'tabela' | 'form'>('tabela')
+
+  const clientes = [
+    new Cliente('Ana',34,'1'),
+    new Cliente('Bia',45,'2'),
+    new Cliente('Carlos',23,'3'),
+    new Cliente('Pedro',54,'4')
+
+  ]
+  
+  function clienteSelecionado(cliente: Cliente){
+      setCliente(cliente)
+      setVisivel('form')
+  }
+
+  function clienteExcluido(cliente: Cliente){
+    console.log(`Excluir... ${cliente.nome}`)
+}
+
+  function salvarCliente(cliente:Cliente){
+    console.log(cliente)
+    setVisivel('tabela')
+  }
+
+  function novoCliente(cliente: Cliente){
+    setCliente(Cliente.vazio())
+    setVisivel('form')
+  }
+
+
   return (
     <div className={`
         flex justify-center items-center h-screen
@@ -8,7 +45,29 @@ export default function Home() {
         text-white
     `}>
       <Layout titulo="cadastro simples">
-        <span>Conteudo</span>
+        {visivel === 'tabela' ?(
+               <>
+               <div className="flex justify-end">
+                   <Botao cor = "green" className="mb-4"
+                      onClick={novoCliente}>
+                     Novo Cliente
+                    </Botao>
+               </div>
+               <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} 
+               clienteExcluido={clienteExcluido}
+           />
+           </>
+        ):(
+          <Formulario 
+            cliente={cliente} 
+            clienteMudou = {salvarCliente}
+            cancelado ={()=>setVisivel('tabela')}
+          
+          />
+
+        )}
+     
+
       </Layout>
 
     </div>
